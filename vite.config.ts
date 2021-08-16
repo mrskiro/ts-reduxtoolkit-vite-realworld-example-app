b8e9b7b04473b7514a3f8a28c056e006e9aa1ac4
purpleeeee <h.purpleeeee@gmail.com>
2021-08-17 00:43:36 +0900

    chore: alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+    plugins: [reactRefresh()],
+    root: './',
+    resolve: {
+        alias: {
+            '~/': '/src/'
+        }
+    }
 })
