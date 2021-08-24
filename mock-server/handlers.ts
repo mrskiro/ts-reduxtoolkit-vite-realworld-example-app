213667a40c32fe1d482524480d62b5332dd51d53
purpleeeee <h.purpleeeee@gmail.com>
2021-08-25 01:00:29 +0900

    fix: handlers

--- mock-server/handlers.ts
+++ mock-server/handlers.ts
@@ -6,6 +6,7 @@ const makePath = (path: string) => `${endpoint}${path}`
 
 export const handlers = [
     rest.post(makePath('/users'), (req, res, ctx) => {
+        sessionStorage.setItem('is-authenticated', 'true')
         console.log(req, res, ctx)
         return res(
             ctx.status(200),
@@ -23,7 +24,18 @@ export const handlers = [
     rest.post(makePath('/users/login'), (req, res, ctx) => {
         sessionStorage.setItem('is-authenticated', 'true')
 
-        return res(ctx.status(200))
+        return res(
+            ctx.status(200),
+            ctx.json({
+                user: {
+                    email: 'jake@jake.jake',
+                    token: 'jwt.token.here',
+                    username: 'jake',
+                    bio: 'I work at statefarm',
+                    image: null
+                }
+            })
+        )
     }),
     rest.get(makePath('user'), (req, res, ctx) => {
         const isAuthenticated = sessionStorage.getItem('is-authenticated')
