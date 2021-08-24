470bcaea65cca99120327c26fa3d990a9b5b0c51
purpleeeee <h.purpleeeee@gmail.com>
2021-08-25 00:33:56 +0900

    fix: header & footer

--- src/components/recipes/Footer/index.tsx
+++ src/components/recipes/Footer/index.tsx
@@ -4,15 +4,13 @@ import * as DesignSystem from '~/components/designSystem'
 export const Footer = () => (
     <footer>
         <div className="container">
-            <DesignSystem.Link href="" className="logo-font">
+            <DesignSystem.Link href="/" className="logo-font">
                 conduit
             </DesignSystem.Link>
             <span className="attribution">
                 An interactive learning project from{' '}
-                <DesignSystem.Link href="https://thinkster.io">
-                    Thinkster
-                </DesignSystem.Link>
-                . Code &amp; design licensed under MIT.
+                <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
+                licensed under MIT.
             </span>
         </div>
     </footer>
--- src/components/recipes/Header/index.tsx
+++ src/components/recipes/Header/index.tsx
@@ -7,23 +7,23 @@ export const Header = () => (
             <DesignSystem.Logo href="" />
             <ul className="nav navbar-nav pull-xs-right">
                 <li className="nav-item">
-                    <DesignSystem.Link className="nav-link" href="">
+                    <DesignSystem.Link className="nav-link" href="/">
                         Home
                     </DesignSystem.Link>
                 </li>
                 <li className="nav-item">
-                    <DesignSystem.Link className="nav-link" href="">
+                    <DesignSystem.Link className="nav-link" href="/editor">
                         <i className="ion-compose"></i>&nbsp;New Post
                     </DesignSystem.Link>
                 </li>
                 <li className="nav-item">
-                    <DesignSystem.Link className="nav-link" href="">
+                    <DesignSystem.Link className="nav-link" href="/settings">
                         <i className="ion-gear-a"></i>&nbsp;Settings
                     </DesignSystem.Link>
                 </li>
 
                 <li className="nav-item">
-                    <DesignSystem.Link className="nav-link" href="">
+                    <DesignSystem.Link className="nav-link" href="/register">
                         Sign up
                     </DesignSystem.Link>
                 </li>
