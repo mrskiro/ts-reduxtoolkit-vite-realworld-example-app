47a58597694240ed41fb8c48a8f6616d294e3f40
purpleeeee <h.purpleeeee@gmail.com>
2021-09-14 22:54:33 +0900

    fix

--- src/__tests__/__mocks__/comment/comment.ts
+++ src/__tests__/__mocks__/comment/comment.ts
@@ -14,7 +14,7 @@ export const comment1: Entities.Comment = {
 }
 
 export const comment2: Entities.Comment = {
-    id: 2,
+    id: '2',
     createdAt: '2016-02-18T03:22:56.637Z',
     updatedAt: '2016-02-18T03:22:56.637Z',
     body: 'It takes a Jacobian2',
