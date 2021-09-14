8897f2e81b0829416022d0acd8931557cd4c59b3
purpleeeee <h.purpleeeee@gmail.com>
2021-09-14 22:22:39 +0900

    feat: Setting story

---
+++ src/components/snowflakes/Setting/index.stories.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import * as Storybook from '@storybook/react'
+import * as Mock from '~/__tests__/__mocks__'
+
+import { Setting } from './'
+
+export default {
+    title: 'Snowflakes/Setting',
+    component: Setting
+} as Storybook.ComponentMeta<typeof Setting>
+
+const Template: Storybook.ComponentStory<typeof Setting> = args => (
+    <Setting {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+    initialValues: {
+        username: '',
+        bio: '',
+        email: '',
+        image: ''
+    },
+    onSubmit: () => {}
+}
+
+export const Edit = Template.bind({})
+Edit.args = {
+    ...Default.args,
+    initialValues: {
+        username: Mock.profile.username,
+        bio: Mock.profile.bio,
+        email: 'jake@jake.jake',
+        image: Mock.profile.image
+    }
+}
